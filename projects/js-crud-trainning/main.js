b1ce2780755e6b6c0434ef87a7f8200af0fef867
nhattien7701vn <nhattien7701vn@gmail.com>
2023-04-26 14:21:14 +0700

    render blog list

--- projects/js-crud-trainning/main.js
+++ projects/js-crud-trainning/main.js
@@ -1,24 +1,40 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+const apiUrl = "https://js-post-api.herokuapp.com/api/posts?_page=1&_limit=6";
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+async function getPostList() {
+  try {
+    const response = await fetch(apiUrl);
+    const data = response.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+}
 
-setupCounter(document.querySelector('#counter'))
+(async () => {
+  const { data } = await getPostList();
+  renderItems(data);
+})();
+
+function renderItems(data) {
+  const blogTemplate = document.querySelector("#blogTemplate");
+  const blogSection = document.querySelector("#blogSection");
+  for (let i = 0; i < data.length; i++) {
+    const cloneBlogTemplate = blogTemplate.cloneNode(true).content;
+
+    const blogCard = cloneBlogTemplate.querySelector(".blog-card")
+    const blogCardImg = cloneBlogTemplate.querySelector(".blog-card--img");
+    const blogCardTitle = cloneBlogTemplate.querySelector(".blog-card--title");
+    const blogCardDesc = cloneBlogTemplate.querySelector(".blog-card--desc");
+    const blogCardDate = cloneBlogTemplate.querySelector(".blog-card--date");
+
+    
+    blogCard.href = "/detail?id=" + data[i].id 
+    blogCardImg.src = data[i].imageUrl;
+    blogCardImg.alt = data[i].title;
+    blogCardTitle.textContent = data[i].title;
+    blogCardDesc.textContent = data[i].description;
+    blogCardDate.textContent = data[i].createdAt;
+
+    blogSection.appendChild(cloneBlogTemplate);
+  }
+}
