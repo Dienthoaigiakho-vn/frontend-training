622633070ceb3436856e99edc1ec2609f3b76de8
nhattien7701vn <nhattien7701vn@gmail.com>
2023-04-26 16:29:37 +0700

    fetch API using axios

--- projects/js-crud-trainning/main.js
+++ projects/js-crud-trainning/main.js
@@ -1,19 +1,28 @@
 const apiUrl = "https://js-post-api.herokuapp.com/api/posts?_page=1&_limit=6";
+import axios from "axios";
 
-async function getPostList() {
-  try {
-    const response = await fetch(apiUrl);
-    const data = response.json();
-    return data;
-  } catch (error) {
-    console.log(error);
-  }
+// su dung axios thu vien cua JS
+async function getPostList(){
+try {
+  const res = await axios.get(apiUrl);
+  return res.data;
+} catch (error) {
+  console.log(error);
+}
 }
 
-(async () => {
-  const { data } = await getPostList();
-  renderItems(data);
-})();
+
+// su dung fetch API mac dinh cua JS
+// async function getPostList() {
+//   try {
+    
+//     const response = await fetch(apiUrl);
+//     const data = response.json();
+//     return data;
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
 
 function renderItems(data) {
   const blogTemplate = document.querySelector("#blogTemplate");
@@ -38,3 +47,10 @@ function renderItems(data) {
     blogSection.appendChild(cloneBlogTemplate);
   }
 }
+
+(async () => {
+  const { data } = await getPostList();
+  renderItems(data);
+})();
+
+
