e530a4718474f21973e6790e851409b794338a50
nhattien7701vn <nhattien7701vn@gmail.com>
2023-04-28 17:37:56 +0700

    WIP delete post

--- projects/js-crud-trainning/main.js
+++ projects/js-crud-trainning/main.js
@@ -1,21 +1,23 @@
 const apiUrl = "https://js-post-api.herokuapp.com/api/posts?_page=1&_limit=6";
 import axios from "axios";
 
+
 // su dung axios thu vien cua JS
-async function getPostList(){
-try {
-  const res = await axios.get(apiUrl);
-  return res.data;
-} catch (error) {
-  console.log(error);
+async function getPostList() {
+  try {
+    const res = await axios.get(apiUrl);
+    return res.data;
+  } catch (error) {
+    console.log(error);
+  }
 }
-}
+
 
 
 // su dung fetch API mac dinh cua JS
 // async function getPostList() {
 //   try {
-    
+
 //     const response = await fetch(apiUrl);
 //     const data = response.json();
 //     return data;
@@ -30,20 +32,30 @@ function renderItems(data) {
   for (let i = 0; i < data.length; i++) {
     const cloneBlogTemplate = blogTemplate.cloneNode(true).content;
 
-    const blogCard = cloneBlogTemplate.querySelector(".blog-card")
+    const blogCard = cloneBlogTemplate.querySelector(".blog-card");
     const blogCardImg = cloneBlogTemplate.querySelector(".blog-card--img");
     const blogCardTitle = cloneBlogTemplate.querySelector(".blog-card--title");
     const blogCardDesc = cloneBlogTemplate.querySelector(".blog-card--desc");
     const blogCardDate = cloneBlogTemplate.querySelector(".blog-card--date");
-    const blogCardAuthor = cloneBlogTemplate.querySelector(".blog-card--author")
-    const date = new Date(data[i].createdAt)
-    blogCard.href = "/detail?id=" + data[i].id 
+    const blogCardAuthor =
+      cloneBlogTemplate.querySelector(".blog-card--author");
+    const date = new Date(data[i].createdAt);
+    blogCard.href = "/detail?id=" + data[i].id;
     blogCardImg.src = data[i].imageUrl;
     blogCardImg.alt = data[i].title;
     blogCardTitle.textContent = data[i].title;
     blogCardDesc.textContent = data[i].description;
-    blogCardDate.textContent = date.getHours() + ":" + date.getMinutes() + " " + date.getDate() + "/" + (date.getMonth() + 1) + "/" + date.getFullYear();
-    blogCardAuthor.textContent = data[i].author; 
+    blogCardDate.textContent =
+      date.getHours() +
+      ":" +
+      date.getMinutes() +
+      " " +
+      date.getDate() +
+      "/" +
+      (date.getMonth() + 1) +
+      "/" +
+      date.getFullYear();
+    blogCardAuthor.textContent = data[i].author;
 
     blogList.appendChild(cloneBlogTemplate);
   }
@@ -52,6 +64,7 @@ function renderItems(data) {
 (async () => {
   const { data } = await getPostList();
   renderItems(data);
+
+  const btneditBlog = document.querySelector(".btn-editBlog")
+  btneditBlog.addEventListener('click', ()=>{console.log("hi")})
 })();
-
-
--- projects/js-crud-trainning/popup.js
+++ projects/js-crud-trainning/popup.js
@@ -1,6 +1,33 @@
+const apiUrl = "https://js-post-api.herokuapp.com/api/posts?_page=1&_limit=6";
+const btnSave = document.querySelector(".btn-savepost");
+import axios from "axios";
+
 const btnAddPost = document.querySelector("#btnAddPost");
 const btnClosePost = document.querySelector(".btn-close");
+
 let isActive = false;
+
+async function createPost() {
+  const title = document.querySelector("#title").value;
+  const author = document.querySelector("#author").value;
+  const desc = document.querySelector("#desc").value;
+  const imagePost = document.querySelector("#imagePost").src;
+  const newObject = {
+    title: title,
+    author: author,
+    description: desc,
+    imageUrl: imagePost
+  };
+
+  try {
+    const res = await axios.post(apiUrl + "/posts", newObject);
+    console.log("Object created successfully");
+    console.log(response.data);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
 
 function showPopupAdd() {
   const popupAdd = document.querySelector(".popup-add");
@@ -22,27 +49,34 @@ function closePopup() {
   isActive = !isActive;
 }
 
-function showImageInput() {
-  const btnchangePostImage = document.querySelector(".btn-changePostImage");
-  const imgInput = document.querySelector("#imgInput");
-  const imagePost = document.querySelector("#imagePost");
+// function showImageInput() {
+//   const btnchangePostImage = document.querySelector(".btn-changePostImage");
+//   const imgInput = document.querySelector("#imgInput");
+//   const imagePost = document.querySelector("#imagePost");
 
-  btnchangePostImage.addEventListener("click", () => {
-    const file = imgInput.files[0];
-    const reader = new FileReader();
+//   btnchangePostImage.addEventListener("click", () => {
+//     const file = imgInput.files[0];
+//     const reader = new FileReader();
 
-    reader.addEventListener("load", () => {
-      imagePost.setAttribute("src", reader.result);
-    });
-    if (file) {
-      reader.readAsDataURL(file);
-    }
-  });
-}
+//     reader.addEventListener("load", () => {
+//       imagePost.setAttribute("src", reader.result);
+//     });
+//     if (file) {
+//       reader.readAsDataURL(file);
+//     }
+//   });
+// }
+
+
+
 (() => {
   if (btnAddPost) {
     btnAddPost.addEventListener("click", showPopupAdd);
     btnClosePost.addEventListener("click", closePopup);
-    showImageInput();
+    btnSave.addEventListener("click", createPost);
+
+
+    // showImageInput();
+    // createPost();
   }
 })();
