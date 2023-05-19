d1a6afb49a9d1c1200b1a7cc91067446ba56ca79
nhattien7701vn <nhattien7701vn@gmail.com>
2023-05-19 16:44:30 +0700

    WIP footer

--- projects/clone-info/src/assets/components/Footer/TopFooter/index.jsx
+++ projects/clone-info/src/assets/components/Footer/TopFooter/index.jsx
@@ -5,21 +5,45 @@ const TopFooter = () => {
   return (
     <div className='topFooter-container'>
       <ul className='topFooter-list'>
-        <li>
+        <li className='topFooter-listInfoContact'>
           <h3 className='topFooter-title'>THÔNG TIN LIÊN HỆ</h3>
           <ul className='topFooter-information'>
-            <li><span>Mua Nhanh Online: 19008922 (8h đến 24H mỗi ngày)</span></li>
-            <li><span>Bảo hành - Hỗ trợ kỹ thuật: 0868922247 (9-21H mỗi ngày)</span></li>
-            <li><span>121 Chu Văn An, P26, Quận Bình Thạnh, HCM</span></li>
-            <li><span>1247, Đường 3 Tháng 2, P6, Quận 11, HCM</span></li>
-            <li><span>56 Lê Văn Việt, Phường Hiệp Phú, TP. Thủ Đức</span></li>
-            <li><span>Giờ hoạt động Showroom: 8:30AM - 9:30PM</span></li>
-            <li><a href="">Hỗ trợ khách hàng: hotro@giakho.vn</a></li>
-            <li><a href="">Khách hàng doanh nghiệp: B2B@giakho.vn</a></li>
+            <li>
+              <img src="https://stg.dienthoaigiakho.vn/_next/image?url=%2Ficons%2Ffooter%2Fhotline.png&w=32&q=75" alt="" />
+              <span>Mua Nhanh Online: 19008922 (8h đến 24H mỗi ngày)</span>
+            </li>
+            <li>
+              <img src="https://stg.dienthoaigiakho.vn/_next/image?url=%2Ficons%2Ffooter%2Fhotline.png&w=32&q=75" alt="" />
+              <span>Bảo hành - Hỗ trợ kỹ thuật: 0868922247 (9-21H mỗi ngày)</span>
+            </li>
+            <li>
+              <img src="https://stg.dienthoaigiakho.vn/_next/image?url=%2Ficons%2Ffooter%2Flocation.png&w=32&q=75" alt="" />
+              <span>121 Chu Văn An, P26, Quận Bình Thạnh, HCM</span>
+            </li>
+            <li>
+              <img src="https://stg.dienthoaigiakho.vn/_next/image?url=%2Ficons%2Ffooter%2Flocation.png&w=32&q=75" alt="" />
+              <span>1247, Đường 3 Tháng 2, P6, Quận 11, HCM</span>
+            </li>
+            <li>
+              <img src="https://stg.dienthoaigiakho.vn/_next/image?url=%2Ficons%2Ffooter%2Flocation.png&w=32&q=75" alt="" />
+              <span>56 Lê Văn Việt, Phường Hiệp Phú, TP. Thủ Đức</span>
+            </li>
+            <li>
+              <img src="https://stg.dienthoaigiakho.vn/_next/image?url=%2Ficons%2Ffooter%2Ftime.png&w=32&q=75" alt="" />
+              <span>Giờ hoạt động Showroom: 8:30AM - 9:30PM</span>
+            </li>
+            <li>
+              <img src="https://stg.dienthoaigiakho.vn/_next/image?url=%2Ficons%2Ffooter%2Femail.png&w=32&q=75" alt="" />
+              <a href="">Hỗ trợ khách hàng: hotro@giakho.vn</a>
+            </li>
+            <li>
+              <img src="https://stg.dienthoaigiakho.vn/_next/image?url=%2Ficons%2Ffooter%2Femail.png&w=32&q=75" alt="" />
+              <a href="">Khách hàng doanh nghiệp: B2B@giakho.vn</a>
+            </li>
           </ul>
         </li>
 
-        <li>
+        <li className='topFooter-listInfo'>
           <h3 className='topFooter-title'>THÔNG TIN TRA CỨU</h3>
           <ul className='topFooter-information'>
             <li><a href="">Mua hàng trả góp</a></li>
@@ -35,7 +59,7 @@ const TopFooter = () => {
           </ul>
         </li>
 
-        <li>
+        <li className='topFooter-listInfo'>
           <h3 className='topFooter-title'>CÂU CHUYỆN GIÁ KHO</h3>
           <ul className='topFooter-information'>
             <li><a href="">Giới thiệu về Giá Kho</a></li>
@@ -45,14 +69,25 @@ const TopFooter = () => {
             <li><a href="">Nhật ký Giao Niềm Vui</a></li>
           </ul>
         </li>
-        
-        <li>
+
+        <li className='topFooter-listInfo'>
           <h3 className='topFooter-title'>KẾT NỐI VỚI CHÚNG TÔI</h3>
           <ul className='topFooter-informationImg'>
             <li><a href=""><img src="https://stg.dienthoaigiakho.vn/icons/footer/facebook.png" alt="" /></a></li>
             <li><a href=""><img src="https://stg.dienthoaigiakho.vn/icons/footer/youtube.png" alt="" /></a></li>
             <li><a href=""><img src="https://stg.dienthoaigiakho.vn/icons/footer/zalo.png" alt="" /></a></li>
             <li><a href=""><img src="https://stg.dienthoaigiakho.vn/icons/footer/instagram.png" alt="" /></a></li>
+          </ul>
+          <p className='topFooter-payment'>Phương thức thanh toán</p>
+          <ul className='topFooter-paymentImg'>
+            <li><img src="https://stg.dienthoaigiakho.vn/icons/footer/visa.svg" alt="" /></li>
+            <li><img src="https://stg.dienthoaigiakho.vn/icons/footer/master.svg" alt="" /></li>
+            <li><img src="https://stg.dienthoaigiakho.vn/icons/footer/atm.svg" alt="" /></li>
+            <li><img src="https://stg.dienthoaigiakho.vn/icons/footer/alepay.svg" alt="" /></li>
+            <li><img src="https://stg.dienthoaigiakho.vn/icons/footer/mpos.svg" alt="" /></li>
+            <li><img src="https://stg.dienthoaigiakho.vn/icons/footer/epay.svg" alt="" /></li>
+            <li><img src="https://stg.dienthoaigiakho.vn/icons/footer/vnpay.svg" alt="" /></li>
+            <li><img src="https://stg.dienthoaigiakho.vn/icons/footer/kredivo.svg" alt="" /></li>
           </ul>
         </li>
 
--- projects/clone-info/src/assets/components/Header/Service/index.jsx
+++ projects/clone-info/src/assets/components/Header/Service/index.jsx
@@ -3,10 +3,16 @@ import "./index.css"
 import ServiceItem from './ServiceItem'
 const Service = () => {
   return (
-    <ul className='listServiceItem'> 
-      <ServiceItem imgSrc="https://dienthoaigiakho.vn/_next/image?url=%2Ficons%2Fcommon%2Fthu-cu-header.svg&w=16&q=75" children="Thu Cũ Đổi Mới"></ServiceItem>
-      <ServiceItem imgSrc="https://dienthoaigiakho.vn/_next/image?url=%2Ficons%2Fcommon%2Fthong-tin-header.svg&w=32&q=75" children="Thông tin"></ServiceItem>
-      <ServiceItem imgSrc="https://dienthoaigiakho.vn/_next/image?url=%2Ficons%2Fcommon%2Fgio-hang.svg&w=32&q=75" children="Giỏ Hàng"></ServiceItem>
+    <ul className='listServiceItem'>
+      <li>
+        <ServiceItem imgSrc="https://dienthoaigiakho.vn/_next/image?url=%2Ficons%2Fcommon%2Fthu-cu-header.svg&w=16&q=75" children="Thu Cũ Đổi Mới"></ServiceItem>
+      </li>
+      <li>
+        <ServiceItem imgSrc="https://dienthoaigiakho.vn/_next/image?url=%2Ficons%2Fcommon%2Fthong-tin-header.svg&w=32&q=75" children="Thông tin"></ServiceItem>
+      </li>
+      <li>
+        <ServiceItem imgSrc="https://dienthoaigiakho.vn/_next/image?url=%2Ficons%2Fcommon%2Fgio-hang.svg&w=32&q=75" children="Giỏ Hàng"></ServiceItem>
+      </li>
     </ul>
   )
 }
